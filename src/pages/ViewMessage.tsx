b1a945df40e3716ab8281dc67ee1c682ca3dfbca
anthau <antto.hautamaki@elisanet.fi>
2021-06-14 07:31:00 +0300

    image Modal window now works

--- src/pages/ViewMessage.tsx
+++ src/pages/ViewMessage.tsx
@@ -1,5 +1,12 @@
 import { useState } from 'react';
-import { Message,Message1, getMessage } from '../data/messages';
+import { Message, Message1, getMessage } from '../data/messages';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+
+
 import {
   IonBackButton,
   IonButtons,
@@ -13,23 +20,55 @@ import {
   IonToolbar,
     useIonViewWillEnter,
     IonGrid, IonRow, IonCol,
+    IonButton, IonModal
 } from '@ionic/react';
 import { personCircle } from 'ionicons/icons';
 import { useParams } from 'react-router';
 import './ViewMessage.css';
 const axios = require('axios');
 const chunk = require('chunk');
+
+function counterReducer(state = { www : '' }, action: any) {
+    switch (action.type) {
+        case 'url':
+            return { www: action.www }
+        default:
+    }
+            return state
+    
+}
+
+const store = createStore(counterReducer)
+
+function setModal(state = { visible: false }, action: any) {
+
+    switch (action.type) {
+        case 'set':
+   
+            return { visible: action.visible }
+        default:
+    }
+    return state
+
+}
+const window1 = createStore(setModal)
 const Row = (props: any) => {
     let message = props.data;
+    const [redraw, setRedraw] = useState(false);
     return (
         <IonGrid>
+            <IonModal isOpen={redraw}  >
+                <IonButton onClick={() => setRedraw(false)}>Close window</IonButton>
+                <img src={store.getState().www} />
+            </IonModal>
             <IonRow>
-                {message.map((image: any) => <IonCol><img src={image.thumbnailUrl} /></IonCol>)}
-                
-      
+                {message.map((image: any) =>
+                    <IonCol onClick={() => { store.dispatch({ type: 'url', www: image.url });  setRedraw(true) }} ><img src={image.thumbnailUrl} />
+                    </IonCol>)}
+                     
         </IonRow>
     </IonGrid>)
-
+    
 }
 const Grid1 = (props: any) => {
     let message = props.data;
@@ -37,10 +76,7 @@ const Grid1 = (props: any) => {
   
     return (
         <IonContent>
-
             {dataChunk.map((row: any) => <Row data={row} />) }
-               
-    
     </IonContent>)
 }
 function ViewMessage() {
@@ -55,9 +91,13 @@ function ViewMessage() {
       });
 
   return (
-    <IonPage id="view-message-page">
+      <IonPage id="view-message-page">
+      <Provider store={store}>
       <IonHeader translucent>
-        <IonToolbar>
+              <IonToolbar>
+                  <IonContent>
+                               
+                  </IonContent>
           <IonButtons>
             <IonBackButton text="Inbox" defaultHref="/home"></IonBackButton>
           </IonButtons>
@@ -71,22 +111,19 @@ function ViewMessage() {
               <IonIcon icon={personCircle} color="primary"></IonIcon>
               <IonLabel className="ion-text-wrap">
                               <h2>
-                                  <h3>Album2 .... content</h3>
-                              
+                                  <h3>Album2 </h3>
+
                               </h2>    
               </IonLabel>
             </IonItem>
             <Grid1 data={message} />
           
-                        
-          
-                         
-     
           </>
         ) : (
           <div>Message not found</div>
         )}
-      </IonContent>
+              </IonContent>
+              </Provider>
     </IonPage>
   );
 }
